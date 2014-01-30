f1c161e851daa390caa100c82120b2b6f90fcec5
Qiyu Zhao <zhao248@purdue.edu>
2014-01-30 00:35:19 -0500

    submit 1

---
+++ Spring2014/PA02/answer02.c
@@ -0,0 +1,128 @@
+#include "answer02.h"
+
+size_t my_strlen(const char * str){
+	int i = 0;
+	while(str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int my_countchar(const char * str, char ch){
+	int i = 0;
+	int count = 0;
+	char temp = 1;
+	for(;str[i]!='\0';i++)
+		if(str[i]==ch)
+			count++;
+	return (count);
+}
+
+char * my_strchr(const char * str, int ch){
+	int i = 0;
+	while(1)
+	{
+		if(str[i]==ch)
+			return &str[i];
+		if(str[i]=='\0')
+			return NULL;
+		i++;
+	}
+}
+
+char * my_strrchr(const char * str, int ch){
+	int i = 0;
+	while(str[i] != '\0')
+	{
+		i++;
+	}
+	while(1)
+	{
+		if(str[i]==ch)
+			return &str[i];
+		if(i==0)
+			return NULL;
+		i--;
+	}
+}
+
+char * my_strstr(const char * haystack, const char * needle){
+	int i = 0;
+	int j = 0;
+	if(needle[0]=='\0')
+		return haystack;
+	for(;haystack[i]!='\0';i++)
+	{
+		if(haystack[i]==needle[0])
+		{
+			for(j=0;needle[j]!='\0';j++)
+				if(needle[j]!=haystack[i+j])
+					break;
+			if(needle[j]=='\0')
+				return &haystack[i];
+		}
+	}
+	return NULL;
+}
+
+char * my_strcpy(char * dest, const char * src){
+	while(*(src-1)!='\0')
+		*dest++=*src++;
+	return dest;
+}
+
+char * my_strcat(char * dest, const char * src){
+	size_t lengthd = 0;
+	size_t lengths = 0;
+	int i = 0;
+	lengths = my_strlen(src);
+	lengthd = my_strlen(dest);
+
+	for (i=lengthd; i <= lengths; ++i)
+	{
+		dest[i] = src[i-lengthd];/* code */
+	}
+	return dest;
+}
+
+int my_isspace(int ch){
+	if (ch == ' ' || ch == '\f' || ch == '\n' || ch == '\r' || ch == '\t' || ch == '\v')
+	{
+		return 1;/* code */
+	}else{
+		return 0;
+	}
+}
+
+int my_atoi(const char * str){
+	int ret = 0;
+	size_t length = 0;
+	int minus = 0;
+	int i = 0;
+	length = my_strlen(str);
+
+	for (; i < length; ++i)
+	{
+		if (my_isspace(str[i])){
+		}else{
+			if (str[i] == '-')
+			{
+				minus = 1;/* code */
+			}else if (str[i] >= '0' && str[i] <= '9')
+			{
+				ret *= 10;
+				ret += (str[i] - '0');/* code */
+			}
+		}
+
+		/* code */
+	}
+	if (minus == 1)
+	{
+		return(-ret);/* code */
+	}else{
+		return(ret);
+	}
+	
+}
